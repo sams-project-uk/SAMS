b3f3af20278adda370104b0871e823176c60e19c
c.s.brady <c.s.brady@warwick.ac.uk>
2026-03-30 10:54:41 +0100

    Added missing files

---
+++ src/InitialConditions/LARE/instantiate.h
@@ -0,0 +1,3 @@
+    #define EOS_DEF(value) template class LARE3DInitialConditions<value>;
+    EOS_DENSITY_ENERGY
+    #undef EOS_DEF
---
+++ src/PhysicsPackages/LARE/instantiate.h
@@ -0,0 +1,3 @@
+#define EOS_DEF(value) template class LARE::LARE3D<value>;
+EOS_DENSITY_ENERGY
+#undef EOS_DEF
