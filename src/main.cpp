9ba92fb81bd27d362b81c5cc3afa5ae74c2bd69f
AstroSnow <b.snow@exeter.ac.uk>
2026-06-15 13:23:18 +0100

    BlastWave3D initial condition

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@
 #include "InitialConditions/SodNeutral/SodShockTube.h"
 #include "InitialConditions/TwoFluidTests/TwoFluidTest.h"
 #include "InitialConditions/Corrugation/Corrugation.h"
+#include "InitialConditions/BlastWave3D/BlastWave3D.h"
 
 #include "LARE3DSingleTemperature/shared_data.h"
 #include "LARE3DNeutralFluid/shared_data.h"
@@ -46,7 +47,7 @@ int main(int argc, char *argv[]){
     //Create and initialize the runner
     SAMS::runner<LARE::LARE3D<>, LARE::LARE3DST<>, LARE::LARE3DNF<>, TWOFLUID::PIP<>, LARE::LARE3DInitialConditions<>, examples::SodShockTube<>, examples::BrioAndWu<>,
 		    examples::MHDRotor<>, examples::OrszagTang<>, examples::OrszagTang3D<>, examples::EmeryWindTunnel<>, 
-        examples::KarmanVortex<>, examples::Corrugation<>, examples::TwoFluidTest > runner;
+        examples::KarmanVortex<>, examples::Corrugation<>, examples::BlastWave3D<>, examples::TwoFluidTest > runner;
     runner.initialize(argc, argv);
     //Finish welcome message
     SAMS::finishWelcomeMessage();
